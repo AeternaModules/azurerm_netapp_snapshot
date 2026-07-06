9bb9f1d624fd06c050cc31584f247a6c7457f87f
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "netapp_snapshots" {
   description = "All netapp_snapshot resources"
   value       = azurerm_netapp_snapshot.netapp_snapshots
 }
+output "netapp_snapshots_account_name" {
+  description = "List of account_name values across all netapp_snapshots"
+  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.account_name]
+}
+output "netapp_snapshots_location" {
+  description = "List of location values across all netapp_snapshots"
+  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.location]
+}
+output "netapp_snapshots_name" {
+  description = "List of name values across all netapp_snapshots"
+  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.name]
+}
+output "netapp_snapshots_pool_name" {
+  description = "List of pool_name values across all netapp_snapshots"
+  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.pool_name]
+}
+output "netapp_snapshots_resource_group_name" {
+  description = "List of resource_group_name values across all netapp_snapshots"
+  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.resource_group_name]
+}
+output "netapp_snapshots_volume_name" {
+  description = "List of volume_name values across all netapp_snapshots"
+  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.volume_name]
+}
 
