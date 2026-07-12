35ef79a9fdb8fa230e1a29e4705a24f5e7355697
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "netapp_snapshots" {
-  description = "All netapp_snapshot resources"
-  value       = azurerm_netapp_snapshot.netapp_snapshots
-}
 output "netapp_snapshots_account_name" {
-  description = "List of account_name values across all netapp_snapshots"
-  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.account_name]
+  description = "Map of account_name values across all netapp_snapshots, keyed the same as var.netapp_snapshots"
+  value       = { for k, v in azurerm_netapp_snapshot.netapp_snapshots : k => v.account_name }
 }
 output "netapp_snapshots_location" {
-  description = "List of location values across all netapp_snapshots"
-  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.location]
+  description = "Map of location values across all netapp_snapshots, keyed the same as var.netapp_snapshots"
+  value       = { for k, v in azurerm_netapp_snapshot.netapp_snapshots : k => v.location }
 }
 output "netapp_snapshots_name" {
-  description = "List of name values across all netapp_snapshots"
-  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.name]
+  description = "Map of name values across all netapp_snapshots, keyed the same as var.netapp_snapshots"
+  value       = { for k, v in azurerm_netapp_snapshot.netapp_snapshots : k => v.name }
 }
 output "netapp_snapshots_pool_name" {
-  description = "List of pool_name values across all netapp_snapshots"
-  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.pool_name]
+  description = "Map of pool_name values across all netapp_snapshots, keyed the same as var.netapp_snapshots"
+  value       = { for k, v in azurerm_netapp_snapshot.netapp_snapshots : k => v.pool_name }
 }
 output "netapp_snapshots_resource_group_name" {
-  description = "List of resource_group_name values across all netapp_snapshots"
-  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.resource_group_name]
+  description = "Map of resource_group_name values across all netapp_snapshots, keyed the same as var.netapp_snapshots"
+  value       = { for k, v in azurerm_netapp_snapshot.netapp_snapshots : k => v.resource_group_name }
 }
 output "netapp_snapshots_volume_name" {
-  description = "List of volume_name values across all netapp_snapshots"
-  value       = [for k, v in azurerm_netapp_snapshot.netapp_snapshots : v.volume_name]
+  description = "Map of volume_name values across all netapp_snapshots, keyed the same as var.netapp_snapshots"
+  value       = { for k, v in azurerm_netapp_snapshot.netapp_snapshots : k => v.volume_name }
 }
 
